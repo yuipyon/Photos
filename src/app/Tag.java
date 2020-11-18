a67477c344efe2e854a8ea1b28b716ae87af2307
yuipyon <53494094+yuipyon@users.noreply.github.com>
2020-11-18 01:42:15 -0500

    my side

--- src/app/Tag.java
+++ src/app/Tag.java
@@ -1,6 +1,18 @@
 package app;
 
 import java.io.Serializable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
 
 /**
  * @author Karun Kanda
@@ -10,7 +22,7 @@ import java.io.Serializable;
 /**
  * The Tag class is where the tagName and tagValue will be stored for any photo.
  */
-public class Tag implements Serializable{
+public class Tag extends Label implements Serializable {
 	
 	/**
 	 * String tagName stores the name of the tag (e.g: location).
@@ -31,6 +43,59 @@ public class Tag implements Serializable{
 		this.tagName = tagName;
 		this.tagValue = tagValue;
 	}
+	
+	public Tag() {
+		super();
+		initTag();
+	}
+
+	private Tag(String arg0, Node arg1) {
+		super(arg0, arg1);
+	}
+
+	public Tag(String arg0) {
+		super(arg0);
+		initTag();
+	}
+
+	private final void initTag(){
+		Path path = new Path();
+		/**
+		 * you will need to increase the 5 if you want the close button to be big
+		 */
+		path.getElements().addAll(
+				new MoveTo(0,0),new LineTo(5, 5),
+				new MoveTo(5,0), new LineTo(0,5));
+		path.setStyle("-fx-stroke: red;");
+		path.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent paramT) {
+				Node n = Tag.this.getParent();
+				if(n instanceof Pane){//of course it is
+					((Pane)n).getChildren().remove(Tag.this);
+				}
+			}
+		});
+		setPadding(new Insets(3,5,3,5));
+		setGraphic(path);
+		setContentDisplay(ContentDisplay.RIGHT);
+		setGraphicTextGap(8);
+		graphicProperty().addListener(new ChangeListener<Node>() {
+			@Override
+			public void changed(ObservableValue<? extends Node> paramObservableValue,
+					Node paramT1, Node paramT2) {
+				if(paramT2 != path){
+					setGraphic(path);
+				}
+			}
+		});
+
+		setStyle("-fx-background-color: gold; "
+				+ "-fx-border-radius: 3;"
+				+ "-fx-border-color: red;");
+	}
+
 	
 	/**
 	 * getTagName gets the name of the tag.
