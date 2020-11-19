8bc08c4e63048db267839e54be3503e707cd8224
Karun Kanda <kkanda900@gmail.com>
2020-11-18 20:41:48 -0500

    print statements begone

--- src/view/AdminController.java
+++ src/view/AdminController.java
@@ -171,7 +171,7 @@ public class AdminController {
 		int selectedIndex = UserList.getSelectionModel().getSelectedIndex();
 		if(selectedIndex != -1) {
 			User userToRemove = (User) UserList.getSelectionModel().getSelectedItem();
-			System.out.println(userToRemove);
+			//System.out.println(userToRemove);
 			int newSelectedIndex = (selectedIndex == UserList.getItems().size() - 1) ? selectedIndex - 1 : selectedIndex;
 			users.remove(selectedIndex);
 			usernameList = FXCollections.observableList(users);
